4d931996cb289d9085d5310acb756c5c94d857dd
Denis <brokenhandq3@yandex.ru>
2024-06-04 20:56:21 +0300

    add class Player

--- Player.js
+++ Player.js
@@ -1,5 +1,42 @@
 class Player {
-  constructor() {}
+  #stats = {
+    attack: 0,
+    defence: 0,
+    hp: 0,
+  };
+
+  constructor(name, stats) {
+    this.name = name;
+    this.#stats = stats;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getStats() {
+    return {
+      ...this.#stats,
+    };
+  }
+
+  addToStats(statsObject) {
+    if (statsObject.attack) {
+      this.#stats.attack += statsObject.attack;
+    }
+    if (statsObject.defence) {
+      this.#stats.attack += statsObject.defence;
+    }
+    if (statsObject.hp) {
+      this.#stats.attack += statsObject.hp;
+    }
+  }
+
+  describe() {
+    const stats = this.#stats;
+
+    console.log(`Player Stats: HP: ${stats.hp} ATK: ${stats.attack} DEF: ${stats.defence}`);
+  }
 }
 
 export { Player };
